ef87586e0a4532dcdeb94031efdc69f94a0158ee
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2025-01-04 19:29:40 +0200

    chore: added factory and strategy

---
+++ Elevator.Application/Factories/ElevatorFactory.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Abstractions;
+using Domain.Elevators;
+
+namespace Application.Factories;
+
+internal sealed class ElevatorFactory : IElevatorFactory
+{
+    public Elevator CreateElevator(ElevatorType type, int id, int maxCapacity)
+    {
+        return type switch
+        {
+            ElevatorType.Glass => new GlassElevator(id, maxCapacity),
+            ElevatorType.Freight => new FreightElevator(id, maxCapacity),
+            ElevatorType.HighSpeed => new HighSpeedElevator(id, maxCapacity),
+            _ => throw new ArgumentException($"Unknown elevator type: {type}")
+        };
+    }
+}
