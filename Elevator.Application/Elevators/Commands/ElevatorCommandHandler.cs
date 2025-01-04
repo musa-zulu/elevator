b287ab08632a8717e66b56cedc34fb1e7906bd0f
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2025-01-04 19:29:01 +0200

    chore: added elevator command

---
+++ Elevator.Application/Elevators/Commands/ElevatorCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Abstractions;
+using Domain.Buildings;
+using Domain.Elevators;
+using SharedKernel;
+
+namespace Application.Elevators.Commands;
+
+internal sealed class ElevatorCommandHandler(Building building, IDispatchStrategy dispatchStrategy)
+    : ICommandHandler<ElevatorCommand>
+{
+    public async Task<Result> Handle(ElevatorCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var floor = building.GetFloor(request.TargetFloor);
+
+            floor.AddPassengers(request.PassengersWaiting);
+
+            var elevator = await dispatchStrategy.SelectElevatorAsync(building.Elevators, request.TargetFloor, request.PassengersWaiting);
+
+            if (elevator == null)
+                return Result.Failure(ElevatorErrors.NotAvailable());
+
+            elevator.LoadPassengers(request.PassengersWaiting);
+
+            await elevator.MoveToFloorAsync(request.TargetFloor, status =>
+            {
+                Console.WriteLine($"[Real-Time Update]: {status}");
+            });
+
+            elevator.UnloadPassengers(request.PassengersWaiting);
+            floor.RemovePassengerFromQueue();
+            floor.ResetPassengerCount();
+
+            return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure(ElevatorErrors.SomethingWentWrong(ex.Message));
+        }
+    }
+}
