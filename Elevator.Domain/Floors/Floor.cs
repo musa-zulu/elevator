80effe2ab27d37f97d0788575df33cf999c62c50
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2025-01-04 19:27:54 +0200

    chore: added floor logic

---
+++ Elevator.Domain/Floors/Floor.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Floors;
+
+public sealed class Floor(int floorNumber)
+{
+    public int PassengerCount { get; private set; } = 0;
+    public int FloorNumber { get; } = floorNumber;
+    public Queue<int> PassengerQueue { get; private set; } = new Queue<int>();
+    
+    public void AddPassengers(int passengerCount)
+    {
+        if (passengerCount < 0) throw new ArgumentException("Cannot add negative passengers.");
+        PassengerQueue.Enqueue(passengerCount);
+    }
+    public void ResetPassengerCount() => PassengerCount = 0;
+    public int RemovePassengerFromQueue() => PassengerQueue.Dequeue();
+}
