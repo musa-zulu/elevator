9248dee7069e1bd16c47a5233196611f46942f69
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2025-01-04 20:30:14 +0200

    tests: added tests for elevator command

---
+++ ApplicationTests/Elevators/Commands/ElevatorCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Application.Elevators.Commands;
+using FluentAssertions;
+using Xunit;
+
+namespace ApplicationTests.Elevators.Commands;
+
+public class ElevatorCommandTests
+{
+    [Fact]
+    public void ElevatorCommand_Constructor_WithValidArguments_ShouldSetProperties()
+    {
+        // Arrange
+        var targetFloor = 5;
+        var passengersWaiting = 10;
+
+        // Act
+        var command = new ElevatorCommand(targetFloor, passengersWaiting);
+
+        // Assert
+        command.TargetFloor.Should().Be(targetFloor);
+        command.PassengersWaiting.Should().Be(passengersWaiting);
+    }
+}
