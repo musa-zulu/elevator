5332de15e256c6a47b11517b0293ed064a9b40f9
Celumusa Zulu <CelumusaZ@hollywoodbets.net>
2025-01-05 18:31:55 +0200

    chore: added unit tests for floor

--- Elevator.Domain/Elevators/ElevatorErrors.cs
+++ Elevator.Domain/Elevators/ElevatorErrors.cs
@@ -5,14 +5,6 @@ namespace Domain.Elevators;
 
 public static class ElevatorErrors
 {
-    public static Error NotAvailable() => Error.NotAvailable(
-        "Elevators.NotAvailable",
-        $"No available elevators. Please wait...");
-
-    public static Error MaxCapacityReached() => Error.Failure(
-        "Elevators.MaxCapicityReached",
-        $"Maximum capacity reached. Others must use other available elevators.");
-
     public static Error SomethingWentWrong(string message) => Error.Failure(
        "Elevators.Error",
        message);
